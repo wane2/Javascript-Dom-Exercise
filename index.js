0f50020c55ce0f4dacd595d7388f724f51d33503
danny <danny@splitzcityzen@gmail.comgit config --global user.email 123456789+odin@users.noreply.github>
2023-11-23 19:59:45 +0300

    Add files for javascript exercises

---
+++ index.js
@@ -0,0 +1,21 @@
+let msg = document.getElementById("message");
+
+console.log(msg);
+
+let btn = document.getElementsByName("language");
+
+console.log(btn);
+
+let orx = document.getElementsByTagName("h1");
+
+console.log(orx);
+
+let boy = document.getElementsByClassName("joy");
+
+console.log(boy);
+
+let cont = document.getElementById("jump");
+
+let hyt = cont.getElementsByClassName("jolly");
+
+console.log(hyt);

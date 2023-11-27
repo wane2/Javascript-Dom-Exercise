ac09326873027a10f96a43f693d600f428fb303e
danny <danny@splitzcityzen@gmail.comgit config --global user.email 123456789+odin@users.noreply.github>
2023-11-27 19:00:42 +0300

    Add dom exercises for query selection

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-let msg = document.getElementById("message");
-
-console.log(msg);
-
-let btn = document.getElementsByName("language");
-
-console.log(btn);
-
-let orx = document.getElementsByTagName("h1");
-
-console.log(orx);
-
-let boy = document.getElementsByClassName("joy");
-
-console.log(boy);
+let msg = document.querySelectorAll(".joy");
 
 let cont = document.getElementById("jump");
 
-let hyt = cont.getElementsByClassName("jolly");
+console.log(msg);
+ 
+console.log(cont);
 
-console.log(hyt);
+let txt = document.getElementsByClassName("jolly");
+
+console.log(txt.parentNode);
+
+let parent = document.querySelector(".pipe");
+
+console.log(parent.childNodes);
